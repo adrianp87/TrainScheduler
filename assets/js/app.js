1331715b0655f2d8ade121ca3b02888c3187dca5
Adrian Perez <adrian.perez87@gmail.com>
2019-07-24 08:55:24 -0700

    pseudocode

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// Link to Firebase
+
+// Add button to add trains, destination, train time, frequency
+
+//Create variables to grab user  user input and assign to variables
+
+//Create object to hold train data
+//then push to databse
+
+// Create functions to calculate time of arrival
+
+//Ensure page does not refresh
